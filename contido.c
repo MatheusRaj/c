2d30eb89370fb8709ba9fdfa175f89a4c122e631
MatheusRaj <matheusraj5@gmail.com>
2018-10-23 17:57:41 -0300

    Contained Point

---
+++ contido.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct {int x, y;} Tponto;
+
+int contido(Tponto e, Tponto d, Tponto p);
+
+int main(){
+  Tponto esq, dir, point;
+  int n;
+  scanf("%d", &n);
+  while (n--){
+    scanf("%f %f %f %f %f %f", &esq.x, &esq.y, &dir.x, &dir.y, &point.x, &point.y);
+    printf("%s\n", contido(esq, dir, point) ? "Contido" : "Nao-Contido");
+  }
+  return 0;
+}
+
+int contido(Tponto e, Tponto d, Tponto p){
+  return p.x >= e.x && p.x <= d.x && p.y >= e.y && p.y <= d.y ? 1 : 0;
+}
